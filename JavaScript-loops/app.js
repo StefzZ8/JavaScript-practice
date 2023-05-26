2b89f9df64a986db233854af42b81983524aabd3
Stefko1998 <Simjanovski998@gmail.com>
2023-05-26 15:03:58 +0200

    for.. of-added

--- JavaScript-loops/app.js
+++ JavaScript-loops/app.js
@@ -84,9 +84,90 @@
 // console.log("CONGRATS YOU GOT THE SECRET!!");
 
 
-let input = prompt('Hey say something..');
-while (true) {
-    input = prompt(input);
-    if (input.toLowerCase() === 'stop copying me') break;
-}
-console.log('OK YOU WIN!');
+// let input = prompt('Hey say something..');
+// while (true) {
+//     input = prompt(input);
+//     if (input.toLowerCase() === 'stop copying me') break;
+// }
+// console.log('OK YOU WIN!');
+
+
+// for... 0f - LOOP. Mostly used for arrays 
+
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer'];
+
+// for (let sub of subreddits) {
+//     console.log(`Visit reddit.com/r/${sub}`);
+// }
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+// for (let char of "hello world") {
+//     console.log(char);
+// }  
+
+
+
+
+// For...Of Practice
+// Let's practice using for...of. I've provided you with an array called numbers.  Please use a for...of loop to loop over it, and print out the square of each value (the number multiplied by itself).
+
+// NOTES:
+
+// 1. Udemy's coding exercise platform does NOT support the ** operator.
+// 2. You could accomplish this by using a while loop or a for loop, but please use a for...of loop! Unfortunately, I can't enforce the type of loop you use, but please I'm begging you to use a for...of.  I will hunt you down if you don't.  Thank you and have a great day!
+
+// Your output should look like this:
+// 1
+// 4
+// 9
+// 16
+// 25
+// 36
+// 49
+// 64
+// 81
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //DON'T CHANGE THIS LINE PLEASE!
+
+// // WRITE YOUR LOOP BELOW THIS LINE:
+// for (let num of numbers) {
+//     let b = num * num;
+//     console.log(b);
+// }
+
+
+// const testScores = {
+//     keenan: 80,
+//     damon: 67,
+//     kim: 89,
+//     shawn: 91,
+//     marlon: 72,
+//     dwayne: 77,
+//     nadia: 83,
+//     elvira: 97,
+//     diedre: 81,
+//     vonnie: 60
+// }
+
+// for (let person in testScores) {
+//     console.log(`${person} scored: ${testScores[person]}`);
+// }
+
+// let total = 0;
+// let scores = Object.values(testScores);
+// for (let score of scores) {
+//     total += score;
+// }
+// console.log(total / scores.length)
